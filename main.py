0f31a933c8c68bf307226ab9d9b24c84bd05dbc8
yamamomo <violetraisondetre@gmail.com>
2026-02-08 09:32:32 +0900

    feat: generate-text

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-def main():
-    print("Hello from learn-reasoning-model!")
+from src.generate_text_functions import get_device, generate_text_basic
+import torch
+from src.qwen3 import download_qwen3_small
+from pathlib import Path
+from src.qwen3 import Qwen3Tokenizer
+from src.qwen3 import Qwen3Model, QWEN_CONFIG_06_B
 
 
 if __name__ == "__main__":
-    main()
+
+    download_qwen3_small(kind="base", tokenizer_only=False, out_dir="qwen3")
+    tokenizer_path = Path("qwen3") / "tokenizer-base.json"
+    tokenizer = Qwen3Tokenizer(tokenizer_file_path=tokenizer_path)
+
+    model_path = Path("qwen3") / "qwen3-0.6B-base.pth"
+
+    model = Qwen3Model(QWEN_CONFIG_06_B)
+    model.load_state_dict(torch.load(model_path))
+
+    device = get_device()
+    model.to(get_device())
+
+    prompt = "Explain large language models in a single sentence."
+    input_token_ids_tensor = torch.tensor(
+        tokenizer.encode(prompt), device=device
+    ).unsqueeze(0)
+
+    max_new_tokens = 100
+    output_token_ids_tensor = generate_text_basic(
+        model=model,
+        token_ids=input_token_ids_tensor,
+        max_new_tokens=max_new_tokens,
+    )
+    output_text = tokenizer.decode(output_token_ids_tensor.squeeze(0).tolist())
+    print(output_text)
--- src/generate_text_functions.py
+++ src/generate_text_functions.py
@@ -2,29 +2,29 @@ from .qwen3 import KVCache
 import torch
 
 
-def get_devive(enable_tensor_cores=True):
+def get_device(enable_tensor_cores=True):
     if torch.cuda.is_available():
         device = torch.device("cuda")
         print("Using NVIDIA CUDA GPU")
 
         if enable_tensor_cores:
-            major, minor = map(int, torch.__version__.split("."))[:0.2]
+            major, minor = map(int, torch.__version__.split(".")[:2])
             if (major, minor) >= (2, 9):
                 torch.backends.cuda.matmul.fp32_precision = "tf32"
-                torch.backends.cudnn.conv.fp32_percision = "tf32"
+                torch.backends.cudnn.conv.fp32_precision = "tf32"
             else:
                 torch.backends.cuda.matmul.allow_tf32 = True
                 torch.backends.cudnn.allow_tf32 = True
 
-        elif torch.backends.mps.is_available():
-            device = torch.device("mps")
-            print("Using Apple Silicon GPU (MPS)")
+    elif torch.backends.mps.is_available():
+        device = torch.device("mps")
+        print("Using Apple Silicon GPU (MPS)")
 
-        else:
-            device = torch.device("cpu")
-            print("Using CPU")
+    else:
+        device = torch.device("cpu")
+        print("Using CPU")
 
-        return device
+    return device
 
 
 @torch.inference_mode()
@@ -34,7 +34,7 @@ def generate_text_basic(model, token_ids, max_new_tokens, eos_token_id=None):
 
     for _ in range(max_new_tokens):
         out = model(token_ids)[:, -1]
-        next_token = torch.argmax(out, dim=1, keepdin=None)
+        next_token = torch.argmax(out, dim=-1, keepdim=True)
 
         # Stop if all sequences in the batch have generated EOS
         if eos_token_id is not None and next_token.item() == eos_token_id:
@@ -42,3 +42,46 @@ def generate_text_basic(model, token_ids, max_new_tokens, eos_token_id=None):
 
         token_ids = torch.cat([token_ids, next_token], dim=1)
     return token_ids[:, input_length:]
+
+
+@torch.inference_mode()
+def generate_text_basic_cache(model, token_ids, max_new_tokens, eos_token_id=None):
+
+    input_length = token_ids.shape[1]
+    model.eval()
+    cache = KVCache(n_layers=model.cfg["n_layers"])
+    model.reset_kv_cache()
+    out = model(token_ids, cache=cache)[:, -1]
+
+    for _ in range(max_new_tokens):
+        next_token = torch.argmax(out, dim=-1, keepdim=True)
+
+        if eos_token_id is not None and next_token.item() == eos_token_id:
+            break
+
+        token_ids = torch.cat([token_ids, next_token], dim=1)
+        out = model(next_token, cache=cache)[:, -1]
+
+    return token_ids[:, input_length:]
+
+
+def generate_stats(
+    output_token_ids, tokenizer, start_time, end_time, print_tokens=True
+):
+    total_time = end_time - start_time
+    print(f"Time: {total_time:.2f} sec")
+    print(f"{int(output_token_ids.numel() / total_time)} tokens/sec")
+
+    for name, backend in (
+        ("CUDA", getattr(torch, "cuda", None)),
+        ("XPU", getattr(torch, "xpu", None)),
+    ):
+        if backend is not None and backend.is_available():
+            max_mem_bytes = backend.max_memory_allocated()
+            max_mem_gb = max_mem_bytes / (1024**3)
+            print(f"Max {name} memory allocated: {max_mem_gb:.2f} GB")
+            backend.reset_peak_memory_stats()
+
+    if print_tokens:
+        output_text = tokenizer.decode(output_token_ids.squeeze(0).tolist())
+        print(f"\n{output_text}")
